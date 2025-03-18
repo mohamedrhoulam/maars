00dd909234f231ec8ee4422a59a76fb78f14faaf
Mohamed Rhoulam <rhoulamed@gmail.com>
2025-03-18 23:10:48 +0000

    update webpack config

--- maars-ui/webpack.config.js
+++ maars-ui/webpack.config.js
@@ -1,34 +1,44 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+import { join, resolve as _resolve } from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import { fileURLToPath } from "url";
 
-module.exports = {
-    mode: "development",
-    entry: "./src/renderer/index.tsx",
-    target: "electron-renderer",
-    devtool: "source-map",
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: "ts-loader",
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"],
-            },
-        ],
-    },
-    resolve: {
-        extensions: [".tsx", ".ts", ".js"],
-    },
-    output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, "dist"),
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, "src/renderer/index.html"),
-        }),
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = new URL(".", import.meta.url).pathname;
+
+export default {
+  mode: "development",
+  entry: "./src/renderer/index.tsx",
+  target: "electron-renderer",
+  devtool: "source-map",
+  devServer: {
+    static: join(__dirname, "dist"),
+    compress: true,
+    port: 3000,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
     ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".jsx"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: _resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: _resolve(__dirname, "src/renderer/index.html"),
+    }),
+  ],
 };
